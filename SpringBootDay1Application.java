b8fa560c9e115f2d19a3eb3404d815d9c659416b
sayali asabe <sayaliasabe31@gmail.com>
2026-01-21 19:39:53 +0530

    Add files via upload

---
+++ SpringBootDay1Application.java
@@ -0,0 +1,15 @@
+package com.example.Spring_Boot_Day1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDay1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDay1Application.class, args);
+		System.err.println("Application is started.....!!");
+	
+	}
+
+}
---
+++ SpringBootDay1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring_Boot_Day1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDay1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
